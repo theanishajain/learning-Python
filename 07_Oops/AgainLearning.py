2dc5a9e2d38c2d59d8c575994e179349ebe9e8a6
theanishajain <123887402+theanishajain@users.noreply.github.com>
2024-12-19 16:28:19 +0530

    New Files

---
+++ 07_Oops/AgainLearning.py
@@ -0,0 +1,27 @@
+class Atm:
+    def __init__(self):
+        self.pin = ''
+        self.balance=0
+        self.menu()
+    def menu(self):
+        user_input = ('''
+                      Hello, how would you like to proceedd?
+                      1.Enter 1 to create pin.
+                      2.Enter 2 to deposit
+                      3.Enter 3 to withdraw
+                      4.Enter 4 to check balance
+                      5.Enter 5 to to Exit''')
+    
+        if user_input =="1":
+            # set_pin = input("Enter pin")
+            print("Create pin")
+            
+            
+        elif user_input=="2":
+            print("Enter 2 to deposit")
+        
+        elif
+            
+        
+        
+       
---
+++ 08_Projects/speechRecognitionAssistant.py
@@ -0,0 +1,27 @@
+import pyttsx3
+import speech_recognition as sr
+import pyjokes
+import webbrowser
+import datetime
+
+def sptxt():
+    recognizer = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        
+      
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
